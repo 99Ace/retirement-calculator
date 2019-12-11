81657f9d8f88edfdfedc2d7296a2390aab1e733b
Ace Ang <mikar@Aces-MacBook-Air.local>
2019-12-12 00:14:41 +0800

    link to show-chart page upon click on submit button

--- script/script.js
+++ script/script.js
@@ -7,6 +7,13 @@ $(function () {
     let interestRate = 0
     let inflationRate = 0
     let totalAssetsValue = 0
+    
+    $('#show-chart').on('click',function() {
+      alert ('555');
+      window.location.href = "show-chart.html";
+
+    });
+
 
     function currencyFormat(num) {
       return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
@@ -21,6 +28,10 @@ $(function () {
         else {
           $("#year-to-retire").text(yearsToRetirement);
         }
+    }
+    function showChart() {
+      alert ('555')
+      window.location.href = "show-chart.html";
     }
     $("#monthly-expense").change(function(){
       // alert('change');
@@ -138,6 +149,79 @@ $(function () {
 
 })
 
+let myChart = document.getElementById('myChart').getContext('2d');
+
+      //Global Options
+      Chart.defaults.global.defaultFontFamily = 'Lato';
+      Chart.defaults.global.defaultFontSize = 15;
+      Chart.defaults.global.defaultFontColor = 'white';
+      
+
+
+      let massPopChart = new Chart(myChart, {
+        type : 'bar', // bar, horizontalBar, pie, line, doughnut, radar, PolcarArea
+        data : {
+          labels :['Funds Required','Funds Available','Gap'],
+          datasets : [{
+            // label : 'Gold Medals',
+            data : [
+              800000,
+              200000,
+              500000,
+            ],
+            // backgroundColor : 'pink'
+            backgroundColor : [
+              'grey',
+              'blue',
+              'yellow'
+            ],
+            // borderWidth  : 1,
+            // borderColor: 'white',
+            hoverBorderWidth: 3,
+            hoverBorderColor: 'white'
+          }]  
+        },
+        options : {
+          title : {
+            display : 'true',
+            text : 'Seagames Results - Number of Gold',
+            fontSize : 25,
+          },
+          legend : {
+            display : false,
+            position : 'right',
+            labels : {
+              fontColor : 'white',
+              backgroundColor : 'brown',
+
+            }
+           },
+           layout : {
+             padding : {
+               left : 100,
+               right : 50,
+               top : 0,
+               bottom : 0,
+             }
+           },
+           tooltips : {
+             enabled : true // true or false..  display the value when hover over the bar
+           },
+           scales: {
+            yAxes: [{
+                display: true,
+                ticks: {
+                    suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                    // OR //
+                    beginAtZero: true   // minimum value will be 0.
+                }
+            }]
+           }
+    
+        
+        }
+
+      });
     
     
     
